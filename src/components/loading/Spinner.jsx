e1c33989fa41aad61dc823517f56692fe71035dc
saeed pc <saeidsaued@gmail.com>
2024-08-17 01:03:24 +0300

    /

--- src/components/loading/Spinner.jsx
+++ src/components/loading/Spinner.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 const Spinner = () => {
   return (
-    <div className='w-12 max-w-full aspect-square border-white border-b-transparent rounded-full animate-spin border-[5px]' />
-  )
-}
+    <div
+     className="w-12 max-w-full aspect-square border-white border-b-transparent rounded-full animate-spin border-[5px]"></div>
+  );
+};
 
-export default Spinner
+export default Spinner;
